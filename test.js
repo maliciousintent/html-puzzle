e416d2eb1efc4ff7e58d4d6fd326bad6337a6b3f
Mauro Bolis <bolismauro@gmail.com>
2013-07-02 16:10:19 +0200

    absolute url fix

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var grabber = require('./index')
   , fs = require('fs');
 
-grabber.grab('http://www.disco.unimib.it/', function(err, page) {
+grabber.grab('http://stackoverflow.com/questions/10940273/how-can-i-read-a-file-encoded-in-utf-16-in-nodejs', function(err, page) {
   
   fs.writeFile('result.html', page, function (err) {
     if (err) throw err;
